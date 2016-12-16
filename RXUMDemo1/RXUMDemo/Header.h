8edb6cd3b77d6e0dab1a1a35c787a30def17e3c5
srxboys <srxboys@126.com>
2016-12-16 17:31:22 +0800

    优化
    优化

--- RXUMDemo1/RXUMDemo/Header.h
+++ RXUMDemo1/RXUMDemo/Header.h
@@ -10,18 +10,18 @@
 #define Header_h
 
 // 友盟 appkey
-#define UM_Statistics_APPKET  @"5787073267e58e144f0026e4"
+#define UM_Statistics_APPKET  @"xx"
 
 // 微信
-#define WXAPPID      @"wx358b3a0857df5da6"
-#define WXAPPSECRET  @"755c9e39d0bb86e14af965a221750f6b"
+#define WXAPPID      @"xx"
+#define WXAPPSECRET  @"xx"
 
 #define QQAPPID      @""
 #define QQAPPSECRET  @""
 
 //新浪微博
-#define SINA_APPKEY @"2617290677"
-#define SINA_APPSECRET @"7a0f42c09a6d9627338a1986c06d23c9"
+#define SINA_APPKEY @"xx"
+#define SINA_APPSECRET @"xx"
 
 #define OpenUrl_notifaction @"AppDelegateOpenURL_Post_Notifaction"
 
